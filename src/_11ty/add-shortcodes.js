f050532f44379da87afce0987ddbca144aeac139
Steve Lee <you@example.comsteve@opendirective.com>
2020-06-21 09:48:13 +0100

    Fix abc transpose error

--- src/_11ty/add-shortcodes.js
+++ src/_11ty/add-shortcodes.js
@@ -120,14 +120,14 @@ exports.addShortcodes = function (eleventyConfig) {
     return `
 \`\`\`abc
 X: 1
-K: ${key} clef=bass transpose=-8
+K: ${key} clef=bass transpose=-12
 M: ${meter}
 Q: ${tempo}
 L: ${unitnotelength}
 C: ${artist}
 T: ${title}
 R: ${rhythm}
-[I: MIDI=program 32]
+[I: MIDI=program 33]
 ${content}
 \`\`\`
 `

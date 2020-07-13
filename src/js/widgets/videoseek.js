4d2acedb606a52eedfb296a5ec5be1299fd16fc9
Steve Lee <you@example.comsteve@opendirective.com>
2020-07-13 09:03:31 +0100

    impove seek hilighting

--- src/js/widgets/videoseek.js
+++ src/js/widgets/videoseek.js
@@ -9,10 +9,19 @@ function videoSeekList_data(root, videoNum) {
     },
 
     showTime(player) {
-      seekButtons.forEach((button) => {
+      seekButtons.forEach((button, i, ar) => {
         // TODO can we make this more efficient
         const seconds = button.dataset.seconds
-        if (seconds <= player.getCurrentTime()) {
+        const nextSeconds =
+          i == ar.length - 1
+            ? Number.MAX_SAFE_INTEGER
+            : seekButtons[i + 1].dataset.seconds
+        const time = player.getCurrentTime()
+        const played = seconds <= time
+        const current = played && time < nextSeconds
+        if (current) {
+          button.style = 'color:var(--red); font-weight:bold;'
+        } else if (played) {
           button.style = 'color:var(--darkred); font-weight:bold;'
         } else {
           button.style = ''
